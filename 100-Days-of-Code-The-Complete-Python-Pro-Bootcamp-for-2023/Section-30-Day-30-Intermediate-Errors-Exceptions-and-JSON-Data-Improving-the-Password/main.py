e9a7ad5b43ee797e69c2fe438eec254a9445a2d4
Lucas-BlancoLB <113478537+Lucas-BlancoLB@users.noreply.github.com>
2024-01-18 22:02:56 -0300

    Add files via upload

--- 100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-for-2023/Section-30-Day-30-Intermediate-Errors-Exceptions-and-JSON-Data-Improving-the-Password/main.py
+++ 100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-for-2023/Section-30-Day-30-Intermediate-Errors-Exceptions-and-JSON-Data-Improving-the-Password/main.py
@@ -1,141 +1,131 @@
-# fileNotFound
-# with open("a_file.txt", mode="r") as file:
-#     file.read()
+import tkinter as tk
+from tkinter import messagebox
+import ast
+import random
+import pyperclip
+import json
 
-# KeyError
-# a_dictionaru = {"key": "value"}
-# value = a_dictionaru["non_existent_key"]
 
-# IndexError
-# fruit_list = ["Apple", "Banana", "Pear"]
-# fruit = fruit_list[3]
 
-# TypeError
-# text = "abc"
-# print(text + 5)
 
-try:  # Something that might cause an exception
-    pass
-except:  # Do this if there was an exception
-    pass
-else:  # Do this if there were no exceptions
-    pass
-finally:  # Do this no matter what happens
-    pass
-# raise KeyError("str") give a Key_Error that will show up cuz I want so
+FONT_NAME = "Courier"
+entries_list =[]
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#
+
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = [random.choice(letters) for _ in range(nr_letters)] + [random.choice(numbers) for _ in
+                                                                             range(nr_numbers)] \
+                    + [random.choice(symbols) for _ in range(nr_symbols)]
+    print(password_list)
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    print(password)
+    entries_list[2].delete(0, tk.END)
+    entries_list[2].insert(tk.END, password)
+    pyperclip.copy(password)
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def is_ok_to_save():
+    return messagebox.askokcancel(title=entries_list[0].get(), message=f"These are the details entered: \n"
+                                                                        f"Login: {entries_list[1].get()}\n"
+                                                                        f"Password: {entries_list[2].get()}\n"
+                                                                        f"Is it okay to save?")
+
+def saving_password():
+    site, login, code = entries_list[0].get(), entries_list[1].get(), entries_list[2].get()
+    if site and login and code == "":
+        messagebox.showinfo(title="😜", message="can't save empty fields ")
+    else:
+        try:
+            with open("passwords_saved.json", mode="r") as file:
+                passwords_saved = json.load(file)
+        except FileNotFoundError:
+            passwords_saved = {"site":{"login": "password"}}
+            with open("passwords_saved.json", "w") as file:
+                json.dump(passwords_saved, file)
+        # check if the upper KEY is in the dict
+        if site not in passwords_saved:
+            passwords_saved[site] = {}
+            passwords_saved[site][login] = code
+
+        #check if the lower key is in the dict[KEY]
+        elif login not in passwords_saved[site]:
+            print(2)
+            passwords_saved[site][login] = code
+        # if the password is different, then change it
+        elif not code == passwords_saved[site][login]:
+            print(3)
+            passwords_saved[site][login] = code
+        # Else there's already an entry for those inputs
+        else:
+            print(4)
+            messagebox.showinfo(title=site, message="There`s already a entry for that login and password")
+        with open("passwords_saved.json", "w") as json_file:
+            json.dump(passwords_saved, json_file, indent=2)
+
+        entries_list[0].delete(0, tk.END), entries_list[2].delete(0, tk.END)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+root = tk.Tk()
+root.title("Password Manager")
+root.geometry("+720+300")
+root.config( pady=20, padx=20, bg='white')
+
+canvas = tk.Canvas(width=200, height=200, highlightthickness=0, bg='white')
+logo = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 95,image=logo,)
+canvas.grid(row=0, column=0)
+
+
+# In frame
+frame = tk.Frame(master=root, bg='white')
+frame.grid(row=1, column=0)
+
+# texts labels in frame
+lb_txt = [" Website:", '   Login:', "Password:"]
+# x and y position
+pos_txt = [(0, 0), (1, 0), (2, 0)]
 ##
-#
+# Entry strings
+web_ent = ["e.g: YouTube.com", "youremail@gmail.com", ""]
+# Entry positions and column span: x, y, n
+pos_colspan_ent = [(0, 1, 2, 50), (1,1, 2, 50), (2,1, 1, 31)]
 
-# FileNotFound
+# Labels
+for position, msg in zip(pos_txt, lb_txt):
+    x,y = position
+    text_label = tk.Label(master=frame , text=msg, font=(FONT_NAME, 15), bg='white')
+    text_label.grid(row=x, column=y, sticky='w')
 
-try:
-    file = open("a_file.txt")
+# Entries
+for pos_colspan, msg in zip(pos_colspan_ent, web_ent):
+    x, y , n, w = pos_colspan
+    web_entry = tk.Entry(master=frame, width=w, bg='white')
+    web_entry.insert(tk.END, string=msg)
+    web_entry.grid(row=x, column=y, columnspan=n)
+    entries_list.append(web_entry)
 
-except:
-    file = open("a_file.txt", mode="w")
-    file.write("something here")
+entries_list[0].focus()
 
-##
+print(entries_list)
+# Buttons
+new_pass = tk.Button(master=frame, text="Generate Password", bg='white', command=generate_password)
+new_pass.grid(row=2, column=2)
 
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["adsdwqdqw"])  # it won't show the error
-except:
-    file = open("a_file.txt", mode="w")  # it writes file
-    file.write("something here")
-
-##
-
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["adsdwqdqw"])
-except FileNotFoundError:
-    file = open("a_file.txt", mode="w")
-    file.write("something here")
-
-except KeyError as key_message:  # to get what went wrong
-    print(f"that key {key_message} does not exist.")
-
-else:  # it won't work if there's an error in try:
-    content = file.read()
-    print(content)
-
-##
-
-try:
-    file = open("a_file.txt")  # If there's an a_file.txt to read
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["key"])  # KeyError fixed
-except FileNotFoundError:
-    file = open("a_file.txt", mode="w")
-    file.write("something here")
-
-except KeyError as key_message:  # to get what went wrong
-    print(f"that key {key_message} does not exist.")
-
-else:
-    content = file.read()
-    print(content)
-
-finally:  # If it succeeds or its failure. It will run
-    file.close()
-    print("File was closed.")
-    # raise KeyError("This is an error that I made up")
-
-##
-
-height = 3  # float(input("Height: "))
-weight = 2  # int(input("Weight: "))
-
-if height > 3:
-    raise ValueError("Human height shouldn't be over 3 meters.")
-
-bmi = pow(weight / height, 2)
-print(bmi)
-
-#
-##
-#
-
-# Exercise 1
-fruits = ["Chicken and Leek", "Cheese and Onion", "Steak", "Fish", "Shepherd's"]
-
-
-# 🚨 Do not change the code above
-
-# TODO: Catch the exception and make sure the code runs without crashing.
-def make_pie(index):
-    try:
-        fruit = fruits[index]
-        print(fruit + " pie")
-    except IndexError:
-        print("Fruit pie")
-
-
-# 🚨 Do not change the code below
-make_pie(4)
-
-
-# Exercise 2
-# eval() function will create a list of dictionaries using the input
-facebook_posts = [{'Likes': 21, 'Comments': 2}, {'Likes': 13, 'Comments': 2, 'Shares': 1},
-                  {'Likes': 33, 'Comments': 8, 'Shares': 3}, {'Comments': 4, 'Shares': 2},
-                  {'Comments': 1, 'Shares': 1}, {'Likes': 19, 'Comments': 3}]
-
-total_likes = 0
-# TODO: Catch the KeyError exception
-
-
-for post in facebook_posts:
-
-    try:
-        total_likes = total_likes + post['Likes']
-    except KeyError as error:
-        pass
-    # facebook_posts_v2 = [x if "Likes" in x else x.update({"Likes": 0}) for x in facebook_posts] # doesn't return None
-    # facebook_posts_v3 = [x.get("Likes", 0) for x in facebook_posts]
-print(total_likes)
+add_pass = tk.Button(master=frame, text="Add", width=42, bg='white', command=saving_password )
+add_pass.grid(row=3, column=1, columnspan=2)
+root.mainloop()
