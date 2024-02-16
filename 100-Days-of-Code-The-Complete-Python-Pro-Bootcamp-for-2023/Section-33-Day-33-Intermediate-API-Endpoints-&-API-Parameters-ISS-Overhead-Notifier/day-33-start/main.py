2699cd440ee54ea1558e14c11e1d0b0f61c59063
Lucas-BlancoLB <113478537+Lucas-BlancoLB@users.noreply.github.com>
2024-02-15 22:53:17 -0300

    Add files via upload

--- 100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-for-2023/Section-33-Day-33-Intermediate-API-Endpoints-&-API-Parameters-ISS-Overhead-Notifier/day-33-start/main.py
+++ 100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-for-2023/Section-33-Day-33-Intermediate-API-Endpoints-&-API-Parameters-ISS-Overhead-Notifier/day-33-start/main.py
@@ -1,10 +1,44 @@
+import os
+import ast
+from dotenv import load_dotenv
 import requests
+import datetime as dt
 
-with requests.get(url="http://api.open-notify.org/iss-now.json") as response:
-    response.raise_for_status()
-    data = response.json()
-    print(data)  # json data
 
-iss_position = (data["iss_position"]["latitude"], data["iss_position"]["longitude"])
+# with requests.get(url="http://api.open-notify.org/iss-now.json") as response:
+#     response.raise_for_status()
+#     data = response.json()
+#     print(data)  # json data
+#
+# iss_position = (data["iss_position"]["latitude"], data["iss_position"]["longitude"])
+#
+# print(iss_position)
 
-print(iss_position)
+
+##
+
+
+load_dotenv()
+info = ast.literal_eval(os.getenv("INFO"))
+
+
+parameters = {
+    "lat":info[0],
+    "lng":info[1],
+    "tzid":info[2],
+    "formatted":info[3],
+}
+
+with requests.get("https://api.sunrise-sunset.org/json", params=parameters) as response_2:
+    response_2.raise_for_status()
+    # print(response_2.json())
+    data = response_2.json()["results"]
+
+sunrise = " ".join(data["sunrise"].split('T')[1].split(":")[:2])
+sunset = " ".join(data["sunset"].split("T")[1].split(":")[:2])
+print(sunrise, "sunrise", "\n" + sunset, "sunset")
+
+hour = dt.datetime.now().hour
+minute = dt.datetime.now().minute
+
+print(hour, minute, "time")
